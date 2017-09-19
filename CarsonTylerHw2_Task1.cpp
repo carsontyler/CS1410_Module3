c6155dbb67a911aff398191edf4fa5957ae0f2bb
Carson Tyler <carsontyler@mail.weber.edu>
2017-09-19 12:59:50 -0600

    Initial Commit

---
+++ CarsonTylerHw2_Task1.cpp
@@ -0,0 +1,16 @@
+// File: CarsonTylerHw2_Task1.cpp
+// Created by Carson Tyler on 9/19/2017 for CS1410.
+// Copyright ©2017 WSU
+#include <iostream>
+using namespace std;
+// Constants
+
+// Prototypes
+
+// Main Program
+int main(void)
+{
+    
+     return 0;
+}
+// Function Definitions
